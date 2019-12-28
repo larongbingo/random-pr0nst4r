7161a972d8378db312f8222ee94ce8f88dbbb2fb
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2019-12-29 02:05:42 +0800

    chore: remove unused service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from "@nestjs/common";
 
 import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { DatabaseModule } from "./database/database.module";
 import { PornstarModule } from "./pornstar/pornstar.module";
 
 @Module({
   imports: [DatabaseModule, PornstarModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
