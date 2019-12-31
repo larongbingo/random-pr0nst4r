6cb85f5895514ccf116c836bb3ddb211b0b7ba11
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2020-01-01 01:09:17 +0800

    feat: on application load, fetch the entire pornstar data and load to memory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
-import { Module } from "@nestjs/common";
+import { Module, Inject, OnModuleInit } from "@nestjs/common";
+import { PornhubModule, PornhubService } from "random/pornhub";
 
+import { PornstarProviderKey, Pornstar } from "./pornstar/pornstar.model";
 import { AppController } from "./app.controller";
 import { DatabaseModule } from "./database/database.module";
 import { PornstarModule } from "./pornstar/pornstar.module";
 
 @Module({
-  imports: [DatabaseModule, PornstarModule],
+  imports: [DatabaseModule, PornstarModule, PornhubModule],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(
+    private readonly pornhubService: PornhubService,
+    @Inject(PornstarProviderKey) private readonly pornstarRepo: typeof Pornstar,
+  ) {}
+
+  public async onModuleInit() {
+    await this.pornstarRepo.sync({force: true});
+    const pornstars = await this.pornhubService.getDetailedStars();
+    const promises = pornstars.map(async pornstar => await this.pornstarRepo.create(pornstar.star));
+    await Promise.all(promises);
+  }
+}
