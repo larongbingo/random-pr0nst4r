3aa664a114e096f47f71e21b4cf5537a29fc3dd3
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2020-01-04 13:14:15 +0800

    feat: use env vars to access db

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,5 +1,18 @@
 import { Options } from "sequelize/types";
 
 export const DatabaseConfig: Options = {
-  dialect: "sqlite",
+  // @ts-ignore
+  dialect: process.env.DATABASE_DIALECT || "sqlite",
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME || "randompornstar",
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  dialectOptions: {
+    dialectOptions: {
+      options: {
+        encrypt: true,
+      },
+    },
+  },
 };
