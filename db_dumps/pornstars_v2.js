efc8f2997a02bbc2c7b01feec35b57bd148aed1e
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2020-05-29 16:21:41 +0800

    chore: Add create table statement

--- db_dumps/pornstars_v2.js
+++ db_dumps/pornstars_v2.js
@@ -3,21 +3,21 @@ const { readFileSync } = require("fs");
 const rawData = readFileSync("./starsList_v3.json");
 const students = JSON.parse(rawData);
 
-/*
+
+console.log("USE randompornstar; \n");
+
 console.log(`
 CREATE TABLE Pornstars(
   id            INTEGER       PRIMARY KEY AUTO_INCREMENT,
-  name          TEXT,
-  thumbnail     LONGTEXT,
-  url           LONGTEXT,
+  star_name          TEXT,
+  star_thumb     LONGTEXT,
+  star_url           LONGTEXT,
   gender        TEXT,
-  videoCount    INTEGER,
+  videos_count_all    INTEGER,
   createdAt     DATE,
   updatedAt     DATE
 );
-`);*/
-console.log("USE randompornstar; \n");
-
+`);
 students.stars.forEach(pornStar => {
   pornStar.star.star_name = pornStar.star.star_name.replace(/"/g, '\\"');
   pornStar.star.star_name = pornStar.star.star_name.replace(/'/g, "\\'");
