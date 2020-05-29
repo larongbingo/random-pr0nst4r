6a4648c82d10dec3a67d0aa81f1ce4150916d582
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2020-05-29 15:29:18 +0800

    fix: startup too long

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
-import { Module, Inject, OnModuleInit } from "@nestjs/common";
-import { PornhubModule, PornhubService } from "random/pornhub";
+import { Module } from "@nestjs/common";
+import { PornhubModule } from "random/pornhub";
 
-import { PornstarProviderKey, Pornstar } from "./pornstar/pornstar.model";
 import { AppController } from "./app.controller";
 import { DatabaseModule } from "./database/database.module";
 import { PornstarModule } from "./pornstar/pornstar.module";
@@ -11,16 +10,4 @@ import { PornstarModule } from "./pornstar/pornstar.module";
   controllers: [AppController],
   providers: [],
 })
-export class AppModule implements OnModuleInit {
-  constructor(
-    private readonly pornhubService: PornhubService,
-    @Inject(PornstarProviderKey) private readonly pornstarRepo: typeof Pornstar,
-  ) {}
-
-  public async onModuleInit() {
-    await this.pornstarRepo.sync({force: true});
-    const pornstars = await this.pornhubService.getDetailedStars();
-    const promises = pornstars.map(async pornstar => await this.pornstarRepo.create(pornstar.star));
-    await Promise.all(promises);
-  }
-}
+export class AppModule {}
--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -3,5 +3,6 @@ import { Options } from "sequelize/types";
 export const DatabaseConfig: Options = {
   dialect: "mysql",
   username: "root",
-  password: "root"
+  password: "root",
+  database: "randompornstar"
 };
