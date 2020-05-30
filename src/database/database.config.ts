885aeb571ef28b09e147af3e99ebb35f83194a97
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2020-05-30 16:06:43 +0800

    feat: Add host

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -2,6 +2,7 @@ import { Options, Dialect } from "sequelize/types";
 
 export const DatabaseConfig: Options = {
   dialect: process.env.DB_DIALECT as Dialect || "mysql",
+  host: process.env.DB_HOST || "127.0.0.1",
   username: process.env.DB_USERNAME || "root",
   password: process.env.DB_PASSWORD || "root",
   database: "randompornstar"
