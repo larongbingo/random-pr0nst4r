da3df2307b921c324ac09107164fe47e1d27eee5
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2020-05-30 16:04:08 +0800

    feat: use env vars for db config

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,8 +1,8 @@
-import { Options } from "sequelize/types";
+import { Options, Dialect } from "sequelize/types";
 
 export const DatabaseConfig: Options = {
-  dialect: "mysql",
-  username: "root",
-  password: "root",
+  dialect: process.env.DB_DIALECT as Dialect || "mysql",
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "root",
   database: "randompornstar"
 };
